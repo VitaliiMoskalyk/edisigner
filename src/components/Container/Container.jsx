72c0c8f20d0ba4a6073cf2708d99769e05e56ec8
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-09-23 00:32:03 +0300

    Updates

--- src/components/Container/Container.jsx
+++
@@ -1,9 +0,0 @@
-import { Section, Box } from "./Container.styled";
-
-export const Container = ({ children }) => {
-  return (
-    <Section>
-      <Box>{children}</Box>
-    </Section>
-  );
-};
--- src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import ErrorPage from "./ErrorPage/ErrorPage";
-import { Header } from "./Header/Header";
-import { MainContent } from "./MainContent/MainContent";
-import { Projects } from "./Projects/Projects";
-
-export { ErrorPage, Header, MainContent, Projects };
