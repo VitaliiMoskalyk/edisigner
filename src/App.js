aebace7f86d22228f55f66a0f62e363e2be9c7b8
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-09-24 00:01:32 +0300

    Update App.js
    basename opts

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorPage, Header, MainContent, Projects } from "./pages";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Header />,
-    errorElement: <ErrorPage />,
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Header />,
+      errorElement: <ErrorPage />,
 
-    children: [
-      {
-        index: true,
-        element: <MainContent />,
-      },
-      {
-        path: "projects",
-        element: <Projects />,
-      },
-      {
-        path: "contacts",
-        element: <div>contacts</div>,
-      },
-    ],
-  },
-]);
+      children: [
+        {
+          index: true,
+          element: <MainContent />,
+        },
+        {
+          path: "projects",
+          element: <Projects />,
+        },
+        {
+          path: "contacts",
+          element: <div>contacts</div>,
+        },
+      ],
+    },
+  ],
+  { basename: "/edisigner" }
+);
 
 function App() {
-  return (
-    <RouterProvider
-      router={router}
-      fallbackElement={<h1>fallback</h1>}
-      basename="/"
-    />
-  );
+  return <RouterProvider router={router} fallbackElement={<h1>fallback</h1>} />;
 }
 
 export default App;
